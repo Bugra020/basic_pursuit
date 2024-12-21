7de1fc0914b116a6b16e1f0f7e377bcdcb021155
Bugra020 <bugrakaya020@gmail.com>
2024-12-22 02:37:52 +0300

    feature

--- basic_pursuit/action_control.py
+++ basic_pursuit/action_control.py
@@ -9,18 +9,44 @@ class ActionControl(Node):
 
         super().__init__("action_control")
 
-        self.start_pos = [0, 0]
+        self.start_pos = [0.0, 0.0]
         self.pos = self.start_pos
-        self.pos_num = 0
+        self.pos_num = 0.0
+        self.dest = self.start_pos
 
         self.subsciption = self.create_subscription(
             Point, "destination", self.get_destination, 10
         )
 
-        #self.publisher = self.create_publisher(Point, "position", 10)
+        self.publisher_ = self.create_publisher(Point, "position", 10)
+        
+        while self.dest[0] == 0.0 and self.dest[1] == 0.0:
+            self.publish_pos()
 
     def get_destination(self, dest):
         self.get_logger().info(f"new destination: {dest.x},{dest.y}")
+
+        while int(self.pos[0]) != int(dest.x) or int(self.pos[1]) != int(dest.y):
+            self.update_pos(dest.x, dest.y)
+
+    def update_pos(self, dx, dy):
+        if int(self.pos[0]) != int(dx):
+            self.pos[0] += int(dx - self.pos[0]) / abs(dx - self.pos[0])
+        if int(self.pos[1]) != int(dy):
+            self.pos[1] += int(dy - self.pos[1]) / abs(dy - self.pos[1])
+
+        self.publish_pos()
+
+    def publish_pos(self):
+        pos = Point()
+        pos.x = self.pos[0]
+        pos.y = self.pos[1]
+        pos.z = self.pos_num
+        self.pos_num += 1
+
+        self.publisher_.publish(pos)
+        self.get_logger().info(f"{int(pos.z)}th pos: {pos.x},{pos.y}")
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -31,5 +57,6 @@ def main(args=None):
     action_control.destroy_node()
     rclpy.shutdown()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- basic_pursuit/path_planning.py
+++ basic_pursuit/path_planning.py
@@ -10,22 +10,44 @@ class PathPublisher(Node):
     def __init__(self):
         super().__init__("path_publisher")
         self.publishler_ = self.create_publisher(Point, "destination", 10)
-        timer_period = 0.5  # seconds
-        self.timer = self.create_timer(timer_period, self.send_destination)
-        self.dest_num = 0
 
-    def send_destination(self):
-        dest = Point()
-        dest.x = random.random() * 100
-        dest.y = random.random() * 100
-        dest.z = float(self.dest_num)
-
-        self.publishler_.publish(dest)
-        self.get_logger().info(
-            f"published dest :{dest.x:.2f},{dest.y:.2f}. destinaion number:{int(dest.z)}"
+        self.subscription = self.create_subscription(
+            Point, "position", self.send_destination, 10
         )
 
-        self.dest_num += 1
+        self.dest = [0.0, 0.0]
+        self.dest_num = 1
+
+    def check_pos(self, pos):
+        if (int(pos.x) == int(self.dest[0])) and (int(pos.y) == int(self.dest[1])):
+            return True
+        else:
+            return False
+
+    def send_destination(self, pos):
+        self.get_logger().info(f"x:{pos.x},y:{pos.y}")
+        dest = Point()
+        dest.x = self.dest[0]
+        dest.y = self.dest[1]
+
+        if self.check_pos(pos):
+            dest = self.new_dest()
+            self.dest[0] = dest.x
+            self.dest[1] = dest.y
+            self.get_logger().info(
+                f"published dest :{dest.x:.2f},{dest.y:.2f}. destinaion number:{int(dest.z)}"
+            )
+            self.dest_num += 1
+
+            self.publishler_.publish(dest)
+
+    def new_dest(self):
+        dest = Point()
+        dest.x = self.dest[0] = float(int(random.random() * 100))
+        dest.y = self.dest[1] = float(int(random.random() * 100))
+        dest.z = float(self.dest_num)
+
+        return dest
 
 
 def main(args=None):
